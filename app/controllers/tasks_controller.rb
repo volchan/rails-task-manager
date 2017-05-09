fd8ca36613e380556e2e335cf38c0e4d71171ff3
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-05-09 12:28:55 +0200

    controller model and index done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,51 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = Task.all
   end
 
   def show
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    # @task.save ? redirect_to task_path(@task) : render :new
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    # @task.update(task_params) ? redirect_to task_path(@task) : render :edit
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+
+  end
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
